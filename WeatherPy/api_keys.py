a398790180c6f25d2d705527796c6a841f5a3df9
jzhao0626 <walt980626@gmail.com>
2021-02-02 15:04:56 -0500

    Retrieving from API

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "80a32d451290adb69a632e81858cd383"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyACdy8Le7aD9d2UsHnqZZJydSGfJIRcu9g"
